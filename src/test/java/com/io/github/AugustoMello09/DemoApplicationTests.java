bc1ad3c9f2405f1633f3633375838c7834a77765
José Augusto <joseaugusto.mello01@gmail.com>
2022-12-22 10:13:57 -0300

    Projeto criado: Category entity e CategoryResource

---
+++ src/test/java/com/io/github/AugustoMello09/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.github.AugustoMello09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
