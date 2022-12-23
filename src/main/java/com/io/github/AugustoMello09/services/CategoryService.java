fd98f77a29042f19c495d25fa4a53924c226608d
José Augusto <joseaugusto.mello01@gmail.com>
2022-12-23 09:59:59 -0300

    DTO

--- src/main/java/com/io/github/AugustoMello09/services/CategoryService.java
+++ src/main/java/com/io/github/AugustoMello09/services/CategoryService.java
@@ -1,23 +1,27 @@
 package com.io.github.AugustoMello09.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.io.github.AugustoMello09.dto.CategoryDTO;
 import com.io.github.AugustoMello09.entities.Category;
 import com.io.github.AugustoMello09.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
 	}
-	
+
 }
