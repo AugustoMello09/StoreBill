9fa24770a9cec9b74ea135c8a7c15795dc8fe7bb
José Augusto <joseaugusto.mello01@gmail.com>
2022-12-28 08:44:17 -0300

    Product repository, service, resource, GET ok

---
+++ src/main/java/com/io/github/AugustoMello09/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.io.github.AugustoMello09.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.io.github.AugustoMello09.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
